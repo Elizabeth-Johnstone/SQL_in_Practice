f7f5b19bb969e5ea7db5a632f5a4c67ad2b355f1
Elizabeth Johnstone <ewj218@nyu.edu>
2021-11-10 21:54:07 -0500

    last version

--- server/controller.js
+++ server/controller.js
@@ -30,7 +30,10 @@ module.exports = {
 
     approveAppointment: (req, res) => {
         let {apptId} = req.body
-        sequelize.query(`*****YOUR CODE HERE*****
+        sequelize.query(`UPDATE cc_appointments
+        SET approved = true
+        WHERE appt_id = ${apptId};
+
         insert into cc_emp_appts (emp_id, appt_id)
         values (${nextEmp}, ${apptId}),
         (${nextEmp + 1}, ${apptId});
@@ -56,21 +59,27 @@ module.exports = {
         .then(dbRes => res.status(200).send(dbRes[0]))
     },
     getPastAppointments: (req, res) => {
-        sequelize.query(``)
+        sequelize.query(`select a.appt_id, a.date, a.service_type, a.notes, u.first_name, u.last_name 
+        from cc_appointments a
+        join cc_emp_appts ea on a.appt_id = ea.appt_id
+        join cc_employees e on e.emp_id = ea.emp_id
+        join cc_users u on e.user_id = u.user_id
+        where a.approved = true and a.completed = true
+        order by a.date desc;`)
+        .then(dbRes => res.status(200).send(dbRes[0]))
+    },
+    
+    completeAppointment: (req, res) => {
+        let {apptId} = req.body
+
+        sequelize.query(`update cc_appointments set completed = true
+        where appt_id = ${apptId};`)
+            .then(dbRes => res.status(200).send(dbRes[0]))
+            .catch(err => console.log(err))
     }
 }
 
 
-// Next, you’ll be writing a query that’s similar to the triple join query from getUpcomingAppointments, reference it as you write yours. This one will get all of the past appointments.
-
-// Using sequelize.query query your database for the following columns from their respective tables cc_appointments: appt_id, date, service_type, notes. cc_users: first_name, last_name. Reference the getUpcomingAppointments function to see how to join all the information together (you’ll need all the same tables again). Make sure to select only rows where both the approved and completed values are true. And order the results by date with the most recent at the top.
-
-// Handle the promise with .then() passing in a callback: dbRes => res.status(200).send(dbRes[0]) (you can also add a .catch)
-
-// In index.js, comment line 25 back in (this line: app.get('/appt', getPastAppointments))
-
-// In home.js (public folder), comment lines 108 and 109 back in (these lines: getUpcomingAppointments() and getPastAppointments())
-
 // Navigate to home.html in your browser
 
-// Now you should be able to see all the past appointments as well as the pending ones.
+// Your whole page should work now! Once pending appointments are approved, they should move to the upcoming section, and once those are complete they should move to the past section.
